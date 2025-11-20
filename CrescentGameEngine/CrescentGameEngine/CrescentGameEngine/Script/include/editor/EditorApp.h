1c73aa0d2ff5903f862ea3d37dcee196cb4fec43
chenyaoyu <chenyaoyu@aobi.com>
2025-11-20 15:49:17 +0800

    feat：【vulkan入门代码】阶段提交

--- CrescentGameEngine/CrescentGameEngine/CrescentGameEngine/Script/include/editor/EditorApp.h
+++ CrescentGameEngine/CrescentGameEngine/CrescentGameEngine/Script/include/editor/EditorApp.h
@@ -19,27 +19,54 @@ public:
 
 private:
 	GLFWwindow* window;
+	/// <summary>
+	/// vulkan实例
+	/// </summary>
 	VkInstance instance;
+	/// <summary>
+	/// vulkan 物理设备
+	/// </summary>
 	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	/// <summary>
+	/// vulkan 逻辑设备
+	/// </summary>
 	VkDevice device;
+	/// <summary>
+	/// 
+	/// </summary>
+	VkDebugUtilsMessengerEXT debugMessenger;
+	/// <summary>
+	/// vulkan surface实例
+	/// </summary>
+	VkSurfaceKHR surface;
 
+	/// <summary>
+	/// 初始化glfw窗口
+	/// </summary>
 	void initWindow() {
 		glfwInit();
 		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 		window = glfwCreateWindow(
 			EditorAppConst::WIDTH,
 			EditorAppConst::HEIGHT,
-			EditorAppConst::EditorAppName, 
+			EditorAppConst::EDITOR_APP_NAME, 
 			nullptr, nullptr);
 	}
 
+	/// <summary>
+	/// 初始化vulkan环境
+	/// </summary>
 	void initVulkan() {
 		createInstance();
+		setupDebugMessenger();
+		createSurface();
 		pickPhysicalDevice();
 		createLogicalDevice();
 	}
 
-	// 创建Vulkan实例
+	/// <summary>
+	/// 创建Vulkan实例
+	/// </summary>
 	void createInstance() {
 		VkApplicationInfo appInfo{};
 		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -63,6 +90,71 @@ private:
 
 		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
 			throw std::runtime_error("failed to create instance!");
+		}
+	}
+
+	/// <summary>
+	/// 验证层日志输出
+	/// </summary>
+	/// <param name="messageSeverity"></param>
+	/// <param name="messageType"></param>
+	/// <param name="pCallbackData"></param>
+	/// <param name="pUserData"></param>
+	/// <returns></returns>
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
+		std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+		return VK_FALSE;
+	}
+
+	/// <summary>
+	/// 创建验证层的createinfo
+	/// </summary>
+	/// <param name="createInfo"></param>
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+		createInfo = {};
+		// 设置扩展机制：允许在保持核心结构体不变的情况下，为其附加新功能。
+		// 若没设sType：
+		//		Vulkan 无法在解析 pNext 时确定扩展结构体的类型。
+		//		无法正确处理扩展链中的多个结构体。
+		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+		// 设置打印更详细的信息
+		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+		// 设置自定义打印
+		createInfo.pfnUserCallback = debugCallback;
+	}
+
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+		auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+		if (func != nullptr) {
+			return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+		}
+		else {
+			return VK_ERROR_EXTENSION_NOT_PRESENT;
+		}
+	}
+
+	/// <summary>
+	/// 启动验证层
+	/// </summary>
+	void setupDebugMessenger() {
+		if (!EditorAppConst::ENABLE_VALIDATION_LAYERS) return;
+
+		VkDebugUtilsMessengerCreateInfoEXT createInfo;
+		populateDebugMessengerCreateInfo(createInfo);
+
+		if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+			throw std::runtime_error("failed to set up debug messenger!");
+		}
+	}
+
+	/// <summary>
+	/// 创建surface拓展实例
+	/// </summary>
+	void createSurface() {
+		if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create window surface!");
 		}
 	}
 
@@ -99,6 +191,21 @@ private:
 		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
 
 		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader;
+
+		//QueueFamilyIndices indices = findQueueFamilies(device);
+
+		//bool extensionsSupported = checkDeviceExtensionSupport(device);
+
+		//bool swapChainAdequate = false;
+		//if (extensionsSupported) {
+		//	SwapChainSupportDetails swapChainSupport = querySwapChainSupport(device);
+		//	swapChainAdequate = !swapChainSupport.formats.empty() && !swapChainSupport.presentModes.empty();
+		//}
+
+		//VkPhysicalDeviceFeatures supportedFeatures;
+		//vkGetPhysicalDeviceFeatures(device, &supportedFeatures);
+
+		//return indices.isComplete() && extensionsSupported && swapChainAdequate && supportedFeatures.samplerAnisotropy;
 	}
 
 	// 创建逻辑设备
--- CrescentGameEngine/CrescentGameEngine/CrescentGameEngine/Script/include/editor/EditorAppConst.h
+++ CrescentGameEngine/CrescentGameEngine/CrescentGameEngine/Script/include/editor/EditorAppConst.h
@@ -7,9 +7,16 @@ namespace EditorAppConst
 	/// <summary>
 	/// 编辑器app 名称
 	/// </summary>
-	const char* EditorAppName = "CresentGameEngineEditor";
+	const char* EDITOR_APP_NAME = "CresentGameEngineEditor";
 	const int WIDTH = 800;
 	const int HEIGHT = 600;
+
+	// 是否开启验证层
+#ifdef NDEBUG
+	const bool ENABLE_VALIDATION_LAYERS = false;
+#else
+	const bool ENABLE_VALIDATION_LAYERS = true;
+#endif
 };
 
 #endif
